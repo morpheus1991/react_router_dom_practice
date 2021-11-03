63c5ed0c8dea68715c25e4483d3a25f6d405dab9
조민혁 <chohyuk0725@gmail.com>
2021-11-04 03:02:44 +0900

    NavLink

--- src/Profiles.tsx
+++ src/Profiles.tsx
@@ -1,15 +1,21 @@
 import React from "react";
-import { Link, Route } from "react-router-dom";
+import { Link, NavLink, Route } from "react-router-dom";
 import Profile from "./Profile";
 
 const Profiles = () => {
+  const activeStyle = {
+    background: "black",
+    color: "#fff",
+  };
   return (
     <div>
       <div>
         <h3>사용자 목록</h3>
         <ul>
           <li>
-            <Link to="/profiles/person1">person1</Link>
+            <NavLink activeStyle={activeStyle} to="/profiles/person1">
+              person1
+            </NavLink>
           </li>
           <li>
             <Link to="/profiles/person2">person2</Link>

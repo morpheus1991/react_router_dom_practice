8b64107a831a703aa5f38a19fc9c87e42b2c131a
조민혁 <chohyuk0725@gmail.com>
2021-11-04 01:56:18 +0900

    url쿼리

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,27 @@
 import React from "react";
-
-const About = () => {
-  return <div>about</div>;
+import qs from "qs";
+type LocationProps = {
+  location: Location;
+};
+type Location = {
+  pathname: string;
+  search: string;
+  hash: string;
+};
+const About = ({ location }: LocationProps) => {
+  console.log(location);
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true, //문자열 맨 앞의 ? 생략.
+  });
+  const showDetail = query.detail === "true";
+  console.log(query.detail);
+  return (
+    <div>
+      <h1>소개</h1>
+      <p>라우터 연습</p>
+      {showDetail && <p>detail 값이 true</p>}
+    </div>
+  );
 };
 
 export default About;
